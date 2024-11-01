966ef556dce3ab776be4ffd952b365007ee8fa18
lsl233 <lishileiscyw0056@youedata.com>
2024-11-01 22:20:50 +0800

    feat: create tag button move top

--- client/src/new-tab/components/tags-nav.tsx
+++ client/src/new-tab/components/tags-nav.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/lib/ui/button";
-import { Hash, Settings } from "lucide-react";
+import { BookmarkPlus, Settings } from "lucide-react";
 import { TagWithChildrenAndParentAndLevel } from "shared/tag";
 import { CreateTagDialog } from "./create-tag-dialog";
 import { ScrollArea } from "@/lib/ui/scroll-area";
@@ -16,7 +16,12 @@ export const TagsNav = ({ tags, activeTag, setActiveTag }: TagsNavProps) => {
   return (
     <div className="flex flex-col h-full">
       <div className="shrink-0 flex items-center justify-center h-[52px] border-b border-gray-300">
-        <Hash />
+        <CreateTagDialog>
+          <Button variant="ghost" className="w-full h-full">
+            <BookmarkPlus className="w-6 h-6 mr-1" />
+            Create
+          </Button>
+        </CreateTagDialog>
       </div>
       <ScrollArea className="flex-1 w-full">
         <div className="p-2">
@@ -30,8 +35,8 @@ export const TagsNav = ({ tags, activeTag, setActiveTag }: TagsNavProps) => {
           ))}
         </div>
       </ScrollArea>
+      
       <div className="shrink-0 flex items-center justify-center h-[52px] border-t border-gray-300">
-        <CreateTagDialog />
         <SettingDialog>
           <Button variant="ghost" className="w-full h-full">
             <Settings className="w-4 h-4 mr-1" />
