406d313a3670112c40992da633921494c89713f1
lsl233 <ahands233@gmail.com>
2024-12-27 20:47:16 +0800

    fix: 1. Added a loading state when creating a Tag. 2. Fixed the search issue.

--- client/src/lib/ui/combobox.tsx
+++ client/src/lib/ui/combobox.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons";
 
 import { cn } from "@/lib/utils";
-import { Button } from "@/lib/ui/button";
+import { Button } from "@/lib/ui/button-loading";
 import {
   Command,
   CommandEmpty,
@@ -20,7 +20,7 @@ import {
 } from "@/lib/ui/popover";
 import { Badge } from "@/lib/ui/badge";
 import { useCommandState } from "cmdk";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Tag as TagIcon } from "lucide-react";
 import { f } from "@/lib/f";
 
@@ -44,6 +44,7 @@ export const Combobox = React.forwardRef<HTMLDivElement, CommandSelectProps<any>
   disabled = false,
 }, ref) => {
   const [open, setOpen] = React.useState(false);
+  const [creatingOption, setCreatingOption] = useState(false)
   const [internalValues, setInternalValues] = React.useState<string[]>(value);
   const [commandState, setCommandState] = React.useState({
     search: "",
@@ -63,7 +64,7 @@ export const Combobox = React.forwardRef<HTMLDivElement, CommandSelectProps<any>
       : [...internalValues, currentValue];
     setInternalValues(newValues);
     onChange(newValues);
-
+    console.log(newValues, 'new values')
   }, [internalValues, onChange, multiple]);
 
   const handleStateChange = useCallback((count: number, search: string) => {
@@ -71,13 +72,21 @@ export const Combobox = React.forwardRef<HTMLDivElement, CommandSelectProps<any>
   }, []);
 
   const handleCreateOption = React.useCallback(async () => {
-    const createdOption = await onCreate?.(commandState.search);
-    setInternalValues((prev) => {
-      const newValues = [...prev, createdOption.id];
-      onChange(newValues);
-      return newValues;
-    });
-    setOpen(false);
+    try {
+      setCreatingOption(true)
+      const createdOption = await onCreate?.(commandState.search);
+      setInternalValues((prev) => {
+        const newValues = [...prev, createdOption.id];
+        onChange(newValues);
+        return newValues;
+      });
+      setOpen(false);
+    } catch (e) {
+      console.error(e)
+    } finally {
+      setCreatingOption(false)
+    }
+    
   }, [commandState.search, onCreate, onChange]);
 
   return (
@@ -121,6 +130,7 @@ export const Combobox = React.forwardRef<HTMLDivElement, CommandSelectProps<any>
                     onClick={handleCreateOption}
                     variant="secondary"
                     className="w-full"
+                    loading={creatingOption}
                   >
                     <TagIcon className="mr-2 h-4 w-4" />
                     创建“{commandState.search}”标签
@@ -133,6 +143,7 @@ export const Combobox = React.forwardRef<HTMLDivElement, CommandSelectProps<any>
               <CommandItem
                 key={option.id}
                 value={option.id}
+                keywords={[option.name]}
                 onSelect={handleSelect}
               >
                 <div className="flex items-center">
