38490753873961d054a1b5de38516f8d63dd957a
lsl233 <ahands233@gmail.com>
2025-01-06 18:04:57 +0800

    feat: english tips

--- shared/auth.ts
+++ shared/auth.ts
@@ -1,21 +1,21 @@
 import { object, string } from "zod";
 
 export const signInSchema = object({
-  email: string().email({ message: "邮箱格式错误" }),
-  password: string().min(6, { message: "密码至少6位" }),
+  email: string().email(),
+  password: string().min(6),
   guestId: string().optional(),
 });
 
 export const signUpSchema = signInSchema
   .extend({
-    confirmPassword: string().min(6, { message: "密码至少6位" }),
+    confirmPassword: string().min(6),
   })
   .refine(
     (data) => {
       return data.password === data.confirmPassword;
     },
     {
-      message: "两次密码不一致",
+      message: "Passwords do not match. Please try again.",
       path: ["confirmPassword"],
     }
   );
