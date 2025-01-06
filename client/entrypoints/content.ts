8e0e47d37f169fe7465cbf56b7bc1d4456704c1e
lsl233 <ahands233@gmail.com>
2025-01-06 13:39:51 +0800

    feat: background & content

--- client/entrypoints/content.ts
+++ client/entrypoints/content.ts
@@ -1,6 +1,29 @@
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
+  matches: ['<all_urls>'],
   main() {
-    console.log('Hello content.');
+    chrome.runtime.sendMessage({ type: "content-script-injected" });
+    // 将数据发送到 background 脚本
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+      console.log(`[接收消息 content-script ${request.type}]`, request);
+      if (request.type === "get-page-content") {
+        const metaDescription = document.querySelector('meta[name="description"]');
+        const data = {
+          title: document.title,
+          url: window.location.href,
+          description: metaDescription?.getAttribute("content") || "",
+          icon: getFavicon(),
+        }
+        console.log(`[发送消息 content-script ${request.type}]`, data)
+        sendResponse(data)
+      }
+    });
+
+    function getFavicon() {
+      const icon =
+        document.querySelector<HTMLLinkElement>('link[rel="icon"]') ||
+        document.querySelector<HTMLLinkElement>('link[rel="shortcut icon"]') ||
+        document.querySelector<HTMLLinkElement>('link[rel="apple-touch-icon"]');
+      return icon?.href || `/favicon.ico`;
+    }
   },
 });
